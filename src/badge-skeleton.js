41bc5a712bef6334da6e7bc9bb3fc01e6dc2d872
Kus Cámara <kcmr@users.noreply.github.com>
2016-09-11 12:10:07 +0200

    refactor(js): remove unneeded observer in badge-skeleton

--- src/badge-skeleton.js
+++ src/badge-skeleton.js
@@ -1,14 +1,3 @@
 Polymer({
-  is: 'badge-skeleton',
-  properties: {
-    avatarSize: {
-      type: Number,
-      value: 50,
-      observer: '_avatarSizeObserver'
-    }
-  },
-  _avatarSizeObserver: function(size) {
-    this.customStyle['--github-profile-avatar-size'] = size + 'px';
-    this.updateStyles();
-  }
+  is: 'badge-skeleton'
 });
